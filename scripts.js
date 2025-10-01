897c7283f2d5f0356bdba03fb92918558acd318b
Demond Balentine <demond.balentine@atlasschool.com>
2025-10-01 11:05:03 -0700

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// ======= Toast Trigger for Contact Form =======
+function showToast() {
+  const toastElement = document.getElementById('contactToast');
+  const toast = new bootstrap.Toast(toastElement);
+  toast.show();
+}
